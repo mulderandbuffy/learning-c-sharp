6a3733636ad7d8b5524e8d2fc69c00d0c738787d
Laura Henry <laurahenry2409@gmail.com>
2022-04-07 15:10:50 +0100

    Added first C# program

---
+++ NewBeginnings/NewBeginnings/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace NewBeginnings
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+         
+            bool playing = true;
+
+            while (playing) {
+
+                int guesses = 0;
+                bool guessed = false;
+
+                Random rand = new Random();
+                int target = rand.Next(1, 100);
+                Console.WriteLine("I'm thinking of a number...");
+
+                while (!guessed) {
+                    int guess = Convert.ToInt32(Console.ReadLine());
+                    guesses++;
+
+                    if (guess == target)
+                    {
+                        Console.WriteLine("Correct");
+                        Console.WriteLine("Guessed in " + Convert.ToString(guesses) + " tries.");
+                        guessed = true;
+                    }
+                    else if (guess > target)
+                    {
+                        Console.WriteLine("Lower!");
+                    }
+                    else if (guess < target)
+                    {
+                        Console.WriteLine("Higher!");
+                    }
+                }
+
+                Console.WriteLine("Play Again [Y/N]?");
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "n")
+                {
+                    playing = false;
+                }
+            }
+        }
+
+    }
+}
